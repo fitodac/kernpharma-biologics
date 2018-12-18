94e684630e1ec609abafe6d8de125cf97d2f0f31
fitodac <fitodac@gmail.com>
2018-12-18 16:13:53 -0300

    Iconos

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -33,7 +33,11 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+			app.receivedEvent('deviceready');
+			
+			if (cordova.platformId == 'android') {
+				StatusBar.backgroundColorByHexString("#00ba46");
+			}
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
